ba2dbc5cc415ff341527a13e234b14ec65b29546
Ali Aryani <lenesis@proton.me>
2025-09-10 22:41:37 +0330

    signed int, unsigned int

--- 05-integer=modifiers/int-modifiers.cpp
+++ 05-integer=modifiers/int-modifiers.cpp
@@ -2,7 +2,14 @@
 using namespace std
 
 ; int main () {
-    
+    // positive, negative
+    int value1 {100}
+    ; int value2 {-200}
+    // you can also use `signed int` and `unsigned int`
+    ; cout << "Value 1: " << value1 << endl
+    ; cout << "Size of value 1: " << sizeof(value1) << endl
+    ; cout << "Value 2: " << value2 << endl
+    ; cout << "Size of value 2: " << sizeof(value2) << endl
     
     ; return 0 ;
 }
