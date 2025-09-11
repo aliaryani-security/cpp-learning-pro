16725cbc6adcee9f939b872c682e4ed5a27c8fc9
Ali Aryani <lenesis@proton.me>
2025-09-11 08:47:59 +0330

    auto with examples and sizes

--- 09-auto/auto-keyword.cpp
+++ 09-auto/auto-keyword.cpp
@@ -2,7 +2,22 @@
 using namespace std
 
 ; int main () {
-    
-    
+    // with the help of `auto` we can ask the
+    // compiler to deduce the type of variable
+    auto var1 {12}
+    ; auto var2 {12.0}
+    ; auto var3 {12.0f}
+    ; auto var4 {12.0l}
+    ; auto var5 {'c'}
+
+    // int modifiers
+    ; auto var6 {123u}
+    ; auto var7 {123ul}
+    ; auto var8 {123ll}
+
+    ; cout << sizeof(var8) << endl
+        << sizeof(var5) << endl
+        << sizeof (var2) << endl
+
     ; return 0 ;
 }
