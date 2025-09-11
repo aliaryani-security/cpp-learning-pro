0804c7565f1e78993f75e016cdd24aff2be35c20
Ali Aryani <lenesis@proton.me>
2025-09-11 07:07:38 +0330

    define boolean

--- 07-booleans/bools.cpp
+++ 07-booleans/bools.cpp
@@ -2,6 +2,12 @@
 using namespace std
 
 ; int main () {
+    bool red_light {true}
+    ; if (red_light == true) {
+        cout << "Stop!" << endl;
+    } else {
+        cout << "Keep going!" << endl;
+    }
 
     ; return 0 ;
 }
