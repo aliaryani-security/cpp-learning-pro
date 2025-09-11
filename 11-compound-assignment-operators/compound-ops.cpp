cc2dc1a862938d863b3c6041d28ac3db6a5074a1
Ali Aryani <lenesis@proton.me>
2025-09-11 20:48:07 +0330

    finish compound ops

--- 11-compound-assignment-operators/compound-ops.cpp
+++ 11-compound-assignment-operators/compound-ops.cpp
@@ -2,7 +2,23 @@
 using namespace std
 
 ; int main () {
-    
+    int value {45}
+    ; cout << "The value is: " << value << endl
+    ; cout << endl
+    ; value += 5 // i.e. value = value + 5
+    ; cout << "value += 5: " << value << endl
+    ; cout << endl
+    ; value -= 5
+    ; cout << "value -= 5: " << value << endl
+    ; cout << endl
+    ; value *= 5
+    ; cout << "value *= 5: " << value << endl
+    ; cout << endl
+    ; value /= 5
+    ; cout << "value /= 5: " << value << endl
+    ; cout << endl
+    ; value %= 6
+    ; cout << "value %= 6: " << value << endl
 
     ; return 0 ;
 }
