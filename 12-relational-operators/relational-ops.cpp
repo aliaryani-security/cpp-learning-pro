b80b700baaa45e4b50a78ed6311f07af7be02e25
Ali Aryani <lenesis@proton.me>
2025-09-11 21:05:11 +0330

    examples of relational operators

---
+++ 12-relational-operators/relational-ops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std
+
+; int main() {
+    int number1 {70}
+    ; int number2 {35}
+    ; cout << "Number 1: " << number1 << endl
+    ; cout << "Number 2: " << number2 << endl
+    ; cout << endl
+    ; cout << "***Comparing Variables***" << endl
+    
+    ; cout << boolalpha
+    ; cout << "number1 > number2: " << (number1 > number2) << endl
+    ; cout << "number2 <= number1: " << (number2 <= number1) << endl
+    ; cout << "number2 == number1: " << (number1 == number2) << endl
+    ; cout << "number2 != number1: " << (number1 != number2) << endl
+
+    ; return 0 ;
+}
