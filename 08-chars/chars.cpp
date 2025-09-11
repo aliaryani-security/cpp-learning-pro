b18c2f9a902430cca0fe4d011b982a9fe3537ffe
Ali Aryani <lenesis@proton.me>
2025-09-11 07:29:22 +0330

    define chars

--- 08-chars/chars.cpp
+++ 08-chars/chars.cpp
@@ -2,6 +2,11 @@
 using namespace std
 
 ; int main () {
+    char character {'A'}
+    ; char char_ascii {65}
+    ; cout << "character: " << character << endl
+    ; cout << "char_ascii: " << char_ascii << endl
+    ; cout << "char_ascii(int): " << static_cast<int>(char_ascii) << endl
     
     ; return 0 ;
 }
