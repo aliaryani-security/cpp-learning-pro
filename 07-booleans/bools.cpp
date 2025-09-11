652a7f8c00adff845c81276eeeff0d0e4ce3a727
Ali Aryani <lenesis@proton.me>
2025-09-11 07:10:16 +0330

    print out bools

--- 07-booleans/bools.cpp
+++ 07-booleans/bools.cpp
@@ -8,6 +8,10 @@ using namespace std
     } else {
         cout << "Keep going!" << endl;
     }
+    // print out bools:
+    cout << red_light << endl // will return 1, which means true
+    ; cout << boolalpha // std::boolalpha will help us see true instead of 1
+    ; cout << red_light << endl
 
     ; return 0 ;
 }
