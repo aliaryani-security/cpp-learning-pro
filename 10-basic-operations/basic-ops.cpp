30404c7af3b033407ab74eba11f4deaba9317081
Ali Aryani <lenesis@proton.me>
2025-09-11 14:11:54 +0330

    addition, subtraction

--- 10-basic-operations/basic-ops.cpp
+++ 10-basic-operations/basic-ops.cpp
@@ -2,7 +2,16 @@
 using namespace std
 
 ; int main () {
+    int number1 {2}
+    ; int number2 {7}
 
+    // Addition
+    ; int result = number1 + number2
+    ; cout << "Result: " << result << endl
+
+    // subtraction
+    ; result = number2 - number1
+    ; cout << "Result: " << result << endl
 
     ; return 0 ;
 }
