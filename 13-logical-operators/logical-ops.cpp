034954eff6c0af4937a22788b67813ce660b537c
Ali Aryani <lenesis@proton.me>
2025-09-11 23:31:05 +0330

    AND operator

--- 13-logical-operators/logical-ops.cpp
+++ 13-logical-operators/logical-ops.cpp
@@ -2,7 +2,13 @@
 using namespace std
 
 ; int main () {
-    
+    bool a {true}
+    ; bool b {false}
+    ; bool c {true}
+    ; cout << boolalpha
+    // AND
+    ; cout << "a && b: " << (a && b) << endl
+    ; cout << "a && c: " << (a && c) << endl
 
     ; return 0 ;
 }
