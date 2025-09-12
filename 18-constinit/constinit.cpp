d98f16333838a9563eb25762e8ecc72cc866bcda
Ali Aryani <lenesis@proton.me>
2025-09-12 16:00:05 +0330

    define a constinit

--- 18-constinit/constinit.cpp
+++ 18-constinit/constinit.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std
+/*
+    `constinit` is a variable (not a constant) that is
+    evaluated at runtime initialisation to prevent 
+    undefined behaviours in static/global which we will
+    cover later. ready? let's go
+*/
+// * it's always declared before the main()
+; constinit int buffer_size {1024} 
 
 ; int main () {
 
