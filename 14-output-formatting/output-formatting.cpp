6202d9928e18c21fcfcfc691d404947acc475a02
Ali Aryani <lenesis@proton.me>
2025-09-12 07:46:18 +0330

    output formatting -> unformatted tablej

--- 14-output-formatting/output-formatting.cpp
+++ 14-output-formatting/output-formatting.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <iomanip>
 using namespace std
 
 ; int main () {
-    
+    cout << "Unformatted table:" << endl
+    ; cout << "Daniel" << " " << "Grey" << " 25" << endl
+    ; cout << "John" << " " << "Smith" << " 34" << endl
+    ; cout << "stanley" << " " << "Woods" << " 74" << endl
+
+
 
     ; return 0 ;
 }
