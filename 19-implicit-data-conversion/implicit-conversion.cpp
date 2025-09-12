89ef6667fc54f9d0eba3b3a1acf184b8fcf061eb
Ali Aryani <lenesis@proton.me>
2025-09-12 16:38:32 +0330

    implicit conversion -> narrowing conversion

--- 19-implicit-data-conversion/implicit-conversion.cpp
+++ 19-implicit-data-conversion/implicit-conversion.cpp
@@ -15,5 +15,18 @@ using namespace std
     ; cout << "Total price: " << total_price << endl
     ; cout << "size of total price: " << sizeof(total_price) << endl //8
 
+    /*
+    another posibility is narrowing conversion
+    in the example below, double will be converted
+    to int and the floating part will be dropped
+    */
+    ; int x
+    ; double y {33.79}
+    ; x = y // here we go
+    ; cout << "the value of y is: " << y << endl
+    ; cout << "the size of y is: " << sizeof(y) << endl
+    ; cout << "the value of x is: " << x << endl
+    ; cout << "the size of x is: " << sizeof(x) << endl
+
     ; return 0 ;
 }
