623c2f0ea1654987d4312ceb5fdd96253518fda4
Ali Aryani <lenesis@proton.me>
2025-09-12 16:26:48 +0330

    implicit conversion -> example given

--- 19-implicit-data-conversion/implicit-conversion.cpp
+++ 19-implicit-data-conversion/implicit-conversion.cpp
@@ -2,6 +2,13 @@
 using namespace std
 
 ; int main () {
+    double price {45.5}
+    ; int units {10}
+
+    ; double total_price = price * units
+
+    ; cout << "Total price: " << total_price << endl
+    ; cout << "size of total price: " << sizeof(total_price) << endl //8
 
     ; return 0 ;
 }
