901406b6635cd286a35e29c7f4741f386c84c511
Ali Aryani <lenesis@proton.me>
2025-09-12 18:17:23 +0330

    print in binary -> print using <bitset>

--- 21-print-int-in-binary/print-int-in-binary.cpp
+++ 21-print-int-in-binary/print-int-in-binary.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bitset>
 using namespace std
 
 ; int main () {
@@ -7,6 +8,11 @@ using namespace std
     ; cout << "data (decimal): " << showbase << dec << data << endl
     ; cout << "data (octal): " << showbase << oct << data << endl
     ; cout << "data (hex): " << showbase << hex << data << endl
+
+    ; cout << "data (binary): "
+        << bitset<16>(data) /*data is unsigned short and therefore 16 bits*/
+        << endl
+
 
     ; return 0 ;
 }
