f6b26144d46c5530cdc08d5982cafde19acde00c
Ali Aryani <lenesis@proton.me>
2025-09-12 07:52:34 +0330

    output formatting -> formatted table

--- 14-output-formatting/output-formatting.cpp
+++ 14-output-formatting/output-formatting.cpp
@@ -8,7 +8,17 @@ using namespace std
     ; cout << "John" << " " << "Smith" << " 34" << endl
     ; cout << "stanley" << " " << "Woods" << " 74" << endl
 
+    ; cout << endl
+    ; cout << "Formatted Table:" << endl
 
+    ; cout << setw(10) << "First Name:" << setw(10) 
+        << "Last Name:" << setw(5) << "Age:" << endl
+    ; cout << setw(10) << "Daniel" << setw(10)
+        << "Grey" << setw(5) << "25" << endl
+    ; cout << setw(10) << "John" << setw(10)
+        << "Smith" << setw(5) << "34" << endl
+    ; cout << setw(10) << "Stanley" << setw(10)
+        << "Woods" << setw(5) << "74" << endl
 
     ; return 0 ;
 }
