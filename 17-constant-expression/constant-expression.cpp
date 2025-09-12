4643749d3bd46df1590a4150d70b2745475db30c
Ali Aryani <lenesis@proton.me>
2025-09-12 15:26:21 +0330

    constexpr -> first example

--- 17-constant-expression/constant-expression.cpp
+++ 17-constant-expression/constant-expression.cpp
@@ -2,6 +2,12 @@
 using namespace std
 
 ; int main () {
+    // `constexpr` is a constant that is calculated during compile time
+    // and is written in the binary instead of being calculated
+    // everytime that the program is run
+    constexpr double PI {3.141592}
+    ; cout << PI << endl
+
 
     ; return 0 ;
 }
