f463d0ebb34ba41c8d9371575945c708413f3c23
Ali Aryani <lenesis@proton.me>
2025-09-12 17:01:26 +0330

    explicit conversion -> implicit example

--- 20-explicit-data-conversion/explicit-conversion.cpp
+++ 20-explicit-data-conversion/explicit-conversion.cpp
@@ -7,7 +7,14 @@ using namespace std
     be done by you, the developer.
     we do this using `static_cast<T>(var)`
     */
-    
+    double x {16.93}
+    ; double y {70.14}
+    // implicit way:
+    ; int sum = x + y
+    ; cout << "sum: " << sum << endl
+    ; cout << "size of sum: " << sizeof(sum) <<endl
+    ; cout << endl
+
 
     ; return 0 ;
 }
