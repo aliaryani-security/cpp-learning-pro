0a07089fcf1edeaf740a84ff52e58f440289fe5a
Ali Aryani <lenesis@proton.me>
2025-09-12 08:00:45 +0330

    output formatting -> formatted table -> justify left; add final comment

--- 14-output-formatting/output-formatting.cpp
+++ 14-output-formatting/output-formatting.cpp
@@ -20,5 +20,24 @@ using namespace std
     ; cout << setw(10) << "Stanley" << setw(10)
         << "Woods" << setw(5) << "74" << endl
 
+    // the above table was right justified by default,
+    // let's change it a little bit
+
+    ; cout << endl
+    ; cout << "Left Justified Formatted Table:" << endl
+
+    ; cout << left
+    ; cout << setw(10) << "First Name:" << setw(10) 
+        << "Last Name:" << setw(5) << "Age:" << endl
+    ; cout << setw(10) << "Daniel" << setw(10)
+        << "Grey" << setw(5) << "25" << endl
+    ; cout << setw(10) << "John" << setw(10)
+        << "Smith" << setw(5) << "34" << endl
+    ; cout << setw(10) << "Stanley" << setw(10)
+        << "Woods" << setw(5) << "74" << endl
+    
+    // there are many more of these, check the documentation
+    // of <iomanip> and <ios> to learn more.
+
     ; return 0 ;
 }
