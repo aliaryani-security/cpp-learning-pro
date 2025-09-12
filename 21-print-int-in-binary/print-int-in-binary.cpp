7d3036536322e67130c1570a717d7db242741df8
Ali Aryani <lenesis@proton.me>
2025-09-12 18:11:56 +0330

    print in binary -> dec,oct,hex example

--- 21-print-int-in-binary/print-int-in-binary.cpp
+++ 21-print-int-in-binary/print-int-in-binary.cpp
@@ -2,7 +2,11 @@
 using namespace std
 
 ; int main () {
+    unsigned short int data {15}
     
+    ; cout << "data (decimal): " << dec << data << endl
+    ; cout << "data (octal): " << oct << data << endl
+    ; cout << "data (hex): " << hex << data << endl
 
     ; return 0 ;
 }
