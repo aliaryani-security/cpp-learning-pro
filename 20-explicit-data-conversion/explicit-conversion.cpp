a64a9c700630d006a9fab90cff93d8f54b4825e8
Ali Aryani <lenesis@proton.me>
2025-09-12 16:57:04 +0330

    explicit conversion -> explanation

--- 20-explicit-data-conversion/explicit-conversion.cpp
+++ 20-explicit-data-conversion/explicit-conversion.cpp
@@ -2,6 +2,11 @@
 using namespace std
 
 ; int main () {
+    /*
+    explicit conversions are supposed to 
+    be done by you, the developer.
+    we do this using `static_cast<T>(var)`
+    */
     
 
     ; return 0 ;
