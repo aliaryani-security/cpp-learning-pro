e67ff54ddb2fe63bf2a08d03dd1d640d4812c20b
Ali Aryani <lenesis@proton.me>
2025-09-12 14:49:05 +0330

    constants -> example with error example

--- 16-constants/constants.cpp
+++ 16-constants/constants.cpp
@@ -4,7 +4,9 @@ using namespace std
 ; int main() {
     // a constant is initialised like a variable,
     // but can never be changed after declaration
-    
+    const int age {35}
+    // ; age = 54 // ! error: assignment of read-only variable ‘age’
+    ; cout << "Age (which is a constant): " << age << endl
 
     ; return 0 ;
 }
