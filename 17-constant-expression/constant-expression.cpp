c36858ad0ae9cd468539eb246ab03d8a9cbe8244
Ali Aryani <lenesis@proton.me>
2025-09-12 15:30:08 +0330

    constexpr -> static_assert()

--- 17-constant-expression/constant-expression.cpp
+++ 17-constant-expression/constant-expression.cpp
@@ -7,7 +7,13 @@ using namespace std
     // everytime that the program is run
     constexpr double PI {3.141592}
     ; cout << PI << endl
-
+    
+    // we can write a condition to check if something
+    // like a library has the version needed for our program
+    ; constexpr double EXAMPLE_LIB_VER {1.23}
+    // and here is the condition:
+    ; static_assert(EXAMPLE_LIB_VER == 1.23)
+    // ; static_assert (EXAMPLE_LIB_VER == 1.24) // ! error: static assertion failed
 
     ; return 0 ;
 }
