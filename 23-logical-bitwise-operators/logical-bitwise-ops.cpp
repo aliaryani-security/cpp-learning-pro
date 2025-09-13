643b1d7dffc7c5901534a86bc2a516b84a1603a7
Ali Aryani <lenesis@proton.me>
2025-09-13 13:30:39 +0330

    logical bitwise ops -> declare and print out value1 and value2

--- 23-logical-bitwise-operators/logical-bitwise-ops.cpp
+++ 23-logical-bitwise-operators/logical-bitwise-ops.cpp
@@ -4,7 +4,16 @@
 using namespace std
 
 ; int main() {
-    
+    int COLUMN_WIDTH {20}
+    ; unsigned char value1 {0x3} // 0000 0011
+    ; unsigned char value2 {0x5} // 0000 0101
+
+    ; cout << setw(COLUMN_WIDTH) << "value 1: "
+        << setw(COLUMN_WIDTH) << bitset<8>(value1) 
+        << endl
+    ; cout << setw(COLUMN_WIDTH) << "value 2: " 
+        << setw(COLUMN_WIDTH) << bitset<8>(value2)
+        << endl
 
     ; return 0 ;
 }
