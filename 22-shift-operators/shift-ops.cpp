901d9e6f1a202e320d11e8751087ad78846c7641
Ali Aryani <lenesis@proton.me>
2025-09-13 08:02:41 +0330

    shift ops -> shift left again

--- 22-shift-operators/shift-ops.cpp
+++ 22-shift-operators/shift-ops.cpp
@@ -15,7 +15,11 @@ using namespace std
     ; value = static_cast<unsigned short int>(value << 1)
     ; cout << "value: " << bitset<16>(value) << endl // 0001111111100000
         << "value in decimal: " << value << endl // 8160
-    
+    // let's shift left by one bit, again
+    ; cout << "left shifting, again" << endl
+    ; value = static_cast<unsigned short int>(value << 1)
+    ; cout << "value: " << bitset<16>(value) << endl // 0011111111000000
+        << "value in decimal: " << value << endl // 16320
 
     ; return 0 ;
 }
