1d21bd43475587485c8a150a841cc2712be18988
Ali Aryani <lenesis@proton.me>
2025-09-13 07:47:54 +0330

    shift ops -> include <bitset>, print value in bits

--- 22-shift-operators/shift-ops.cpp
+++ 22-shift-operators/shift-ops.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <bitset>
 using namespace std
 
 ; int main () {
 
     unsigned short int value {0xff0u}
-    ; cout << "Size of short int: " << sizeof(short int) << endl
+    ; cout << "Size of short int: " << sizeof(short int) << endl // 16
     
+    ; cout << "value: " << bitset<16>(value) << endl // 0000111111110000
 
     ; return 0 ;
 }
