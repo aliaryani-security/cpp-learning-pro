032dc5091dc0ac9a91fd0285393aa41492b72f0d
Ali Aryani <lenesis@proton.me>
2025-09-14 19:16:48 +0330

    switch statement -> declare vars

--- 26-switch-statement/switch-statement.cpp
+++ 26-switch-statement/switch-statement.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+const int Pen { 10 };
+const int Marker { 20 };
+const int Eraser { 30 };
+const int Rectangle { 40 };
+const int Circle { 50 };
+const int Ellipse { 60 };
+
 int main () {
+    int tool { Marker };
+
+
 
     return 0;
 }
