15daeadc0388abea6ef8c713310a21cae0d6df26
Ali Aryani <lenesis@proton.me>
2025-09-14 19:48:56 +0330

    if constexpr -> declare condition

--- 28-if-constexpr/if-constexpr.cpp
+++ 28-if-constexpr/if-constexpr.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main () {
+    constexpr bool condition {false};
+
+    if constexpr (condition) {
+        cout << "condition was true" << endl;
+    } else {
+        cout << "condition was false" << endl;
+    }
 
     return 0;
 }
