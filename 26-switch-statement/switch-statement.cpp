c054bd7213b983d3366f4faf9d753aaff589c294
Ali Aryani <lenesis@proton.me>
2025-09-14 19:19:59 +0330

    switch statement -> declare conditions

--- 26-switch-statement/switch-statement.cpp
+++ 26-switch-statement/switch-statement.cpp
@@ -11,7 +11,28 @@ const int Ellipse { 60 };
 int main () {
     int tool { Marker };
 
-
+    switch (tool) {
+        case Pen: {
+            cout << "pen" << endl;
+        } break;
+        case Marker: {
+            cout << "marker" << endl;
+        } break;
+        case Eraser: {
+            cout << "eraser" << endl;
+        } break;
+        case Rectangle: {
+            cout << "rectangle" << endl;
+        } break;
+        case Circle: {
+            cout << "circle" << endl;
+        } break;
+        case Ellipse: {
+            cout << "ellipse" << endl;
+        } break;
+        default:
+            break;
+    }
 
     return 0;
 }
