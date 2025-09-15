8ecefe01dceca7810233fd47608ec46591071f7a
Ali Aryani <lenesis@proton.me>
2025-09-15 19:45:15 +0330

    comma op -> write example

--- 32-comma-operator/comma-op.cpp
+++ 32-comma-operator/comma-op.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int main () {
-    
+    int increment {5};
+    int number1 {10};
+    int number2 {20};
+    int number3 {25};
+
+    int result = (number1 *= ++increment, number2 - (++increment), number3 += ++increment);
+    cout << "number1: " << number1 << endl;
+    cout << "number2: " << number2 << endl;
+    cout << "number3: " << number3 << endl;
+    cout << "result: " << result << endl;
 
     return 0;
 }
