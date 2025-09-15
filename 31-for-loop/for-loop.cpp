a9a1483d7c9202177f134bff9c6351ea28f88a00
Ali Aryani <lenesis@proton.me>
2025-09-15 19:34:47 +0330

    for loop -> multiple declaration

--- 31-for-loop/for-loop.cpp
+++ 31-for-loop/for-loop.cpp
@@ -5,6 +5,11 @@ int main () {
     for (int i {12}; i > 0; i--) {
         cout << i << " I love C++" << endl;
     }
+    
+    // multiple decalration in for loop
+    for (int x {15}, y {20}, z {25}; y < 100; x += 5, y++, z -= 2) {
+        cout << "X: " << x << " Y: " << y << " Z: " << z << endl;
+    }
 
     return 0;
 }
