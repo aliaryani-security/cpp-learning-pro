99e911e247e2799a63a9e6e87f6930400a9ff6e4
Ali Aryani <lenesis@proton.me>
2025-09-15 21:48:40 +0330

    arrays -> read and write data

--- 34-arrays/arrays.cpp
+++ 34-arrays/arrays.cpp
@@ -5,5 +5,21 @@ int main () {
     // declare the list of arrays
     int scores [10];
 
+    // read data
+    cout << "scores [0]: " << scores[0] << endl;
+
+    // write data to array
+    scores[0] = 12;
+    scores[1] = 42;
+    scores[2] = 1;
+    scores[3] = 88;
+    scores[4] = 33;
+    scores[5] = 111;
+    scores[6] = 1234;
+    scores[7] = 2;
+    scores[8] = 222;
+    scores[9] = 783;
+    
+
     return 0;
 }
