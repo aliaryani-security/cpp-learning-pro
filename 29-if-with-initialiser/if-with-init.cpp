022fd15498d5aec46c038a78db186595606b19e7
Ali Aryani <lenesis@proton.me>
2025-09-15 18:14:52 +0330

    if with initialiser -> write an example

--- 29-if-with-initialiser/if-with-init.cpp
+++ 29-if-with-initialiser/if-with-init.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main () {
-    
+    bool go {false};
+
+    if (int speed {10}; go) {
+        cout << "speed: " << speed << endl;
+        if (speed > 5) {
+            cout << "Slow Down!" << endl;
+        } else {
+            cout << "All Good!" << endl;
+        }
+    } else {
+        cout << "Speed: " << speed << endl;
+        cout << "Stop!" << endl;
+    }
+    // ! error: ‘speed’ was not declared in this scope
+    // cout << "Out of the block. speed: " << speed << endl;
 
     return 0;
 }
