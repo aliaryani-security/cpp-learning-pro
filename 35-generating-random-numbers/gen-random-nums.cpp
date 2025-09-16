522c3c102a04f758e0bbc819cffee45b40277f45
Ali Aryani <lenesis@proton.me>
2025-09-16 13:31:00 +0330

    random numbers -> better example

--- 35-generating-random-numbers/gen-random-nums.cpp
+++ 35-generating-random-numbers/gen-random-nums.cpp
@@ -4,6 +4,13 @@ using namespace std;
 int main() {
     int random_number {rand()};
     cout << "random number: " << random_number << endl;
+    // let's do a few of these here
+    cout << endl << "***Start of the for loop***" << endl;
+    int rand_num;
+    for (int i {0}; i < 50; i++) {
+        rand_num = rand();
+        cout << "Random number: " << rand_num << endl;
+    }
 
     return 0;
 }
