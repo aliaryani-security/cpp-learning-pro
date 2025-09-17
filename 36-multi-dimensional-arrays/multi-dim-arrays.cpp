eda0cfdecad6a8bcc369d09b3a674ace7fe70d05
Ali Aryani <lenesis@proton.me>
2025-09-17 21:10:29 +0330

    multi-dimensional arrays -> give an example; print it out

--- 36-multi-dimensional-arrays/multi-dim-arrays.cpp
+++ 36-multi-dimensional-arrays/multi-dim-arrays.cpp
@@ -1,7 +1,19 @@
+#include <cstddef>
 #include <iostream>
 using namespace std;
 
 int main() {
-
+    const size_t name_length{15};
+    char members [][name_length] {
+        "John",
+         "Mark",
+         "Andrew",
+         "Abby"
+    };
+    // printing out members
+    cout << "Printing out members:" << endl;
+    for (size_t i {0}; i < size(members); ++i) {
+        cout << members[i] << endl;
+    }
     return 0;
 }
