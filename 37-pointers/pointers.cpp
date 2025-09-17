a8a8d9d34c2c09843b7e710175b668d9a07220b1
Ali Aryani <lenesis@proton.me>
2025-09-18 01:22:15 +0330

    pointers -> declare; print size

--- 37-pointers/pointers.cpp
+++ 37-pointers/pointers.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
+    // declare and initialise pointers
+    int* p_number {}; // will initialise to nullptr
+    double* p_franctional_number {};
+
+    // pointers are the same size as of vars:
+    cout << "sizeof(int): " << sizeof(int) << endl;
+    cout << "sizeof(p_number): " << sizeof(p_number) << endl;
+    cout << "sizeof(int*): " << sizeof(int*) << endl;
+    cout << "sizeof(double): " << sizeof(double) << endl;
+    cout << "sizeof(p_franctional_number): " << sizeof(p_franctional_number) << endl;
+    cout << "sizeof(double*): " << sizeof(double*) << endl;
+    
 
     return 0;
 }
