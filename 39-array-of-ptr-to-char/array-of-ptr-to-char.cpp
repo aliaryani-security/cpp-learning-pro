4ecae3f709b50e3d916700e875e080fa0ebcd47e
Ali Aryani <lenesis@proton.me>
2025-09-19 00:07:01 +0330

    array of ptr to char -> declare and give example

--- 39-array-of-ptr-to-char/array-of-ptr-to-char.cpp
+++ 39-array-of-ptr-to-char/array-of-ptr-to-char.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
+    // to create an array of strings, we had to use
+    // multi-dimensional arrays, like `char array_of_strings [][90] {...}`
+    // but not anymore. let's do something using pointers!
+    char first_array [][30] {
+        "Hello, how are you?",
+        "Life is sweet, or is it?",
+        "Darkness settles in...",
+    };
+    // and the new way :
+    const char* second_array [] {
+        "Hello, how are you?",
+        "Life is sweet, or is it?",
+        "Darkness settles in..."
+    };
 
     return 0;
 }
