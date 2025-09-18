9b52b92f084e9f3046a3f2fd9ba522f29ba6e23b
Ali Aryani <lenesis@proton.me>
2025-09-18 23:44:58 +0330

    ptr-to-char -> write example

--- 38-pointer-to-char/ptr-to-char.cpp
+++ 38-pointer-to-char/ptr-to-char.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main () {
+    char* message {"Hello World"}; // This is not a good thing to do. C++ gives us a warning.
+    cout << "message: " << message << endl;
+    cout << "*message: " << *message << endl;
 
     return 0;
 }
