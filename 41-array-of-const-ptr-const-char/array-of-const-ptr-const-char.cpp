0f2b080a92b28f02066e4da19dc7c3f8da43daad
Ali Aryani <lenesis@proton.me>
2025-09-20 00:11:42 +0330

    array of const ptr const char -> modify value; print out

--- 41-array-of-const-ptr-const-char/array-of-const-ptr-const-char.cpp
+++ 41-array-of-const-ptr-const-char/array-of-const-ptr-const-char.cpp
@@ -17,6 +17,17 @@ int main () {
         cout << student << endl;
     }
 
+    //*students[0] = 'K'; // compiler error
+
+    // can swap for a new student though
+    const char* new_student {"Bob the Great"};
+    students[0] = new_student;
+    cout << endl;
+    cout << "Printing out the students:" << endl;
+    for (const char* student: students) {
+        cout << student << endl;
+    }
+
     return 0;
 }
 
