da7aa50f36efb12a7e2326d047983defcd7e1d80
Ali Aryani <lenesis@proton.me>
2025-09-19 21:42:18 +0330

    const ptr and ptr to const -> raw variables

--- 40-const-ptr-ptr-to-const/const-ptr.cpp
+++ 40-const-ptr-ptr-to-const/const-ptr.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main () {
+    // a raw variable that can be modified
+    cout << endl;
+    cout << "Raw variable that can be modified: " << endl;
+
+    int number {5}; // not a const, can be changed anytime you want
+    cout << "number: " << number << endl;
+    cout << "&number: " << &number << endl;
+    // modify
+    number = 12;
+    number += 7;
+    cout << "number: " << number << endl;
+    cout << "&number: " << &number << endl;
+    cout << endl;
 
     return 0;
 }
