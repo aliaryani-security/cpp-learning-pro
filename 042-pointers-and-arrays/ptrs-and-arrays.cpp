b83c0ed6b76d94071d458cd90b9b89f7de5a91f3
Ali Aryani <lenesis@proton.me>
2025-09-20 01:07:01 +0330

    pointers and arrays -> declare and print out

--- 042-pointers-and-arrays/ptrs-and-arrays.cpp
+++ 042-pointers-and-arrays/ptrs-and-arrays.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
+    int scores [10] {11,12,13,14,15,16,17,18,19,20};
+    int* p_score {scores};
+
+    // print addresses
+    cout << "scores: " << scores << endl;
+    cout << "p_score: " << p_score << endl;
+
+    // print content at that address
+    cout << endl;
+    cout << "printing data at array address:" << endl;
+    cout << "*scores: " << *scores << endl;
+    cout << "scores[0]: " << scores[0] << endl;
+    cout << "*p_score: " << *p_score << endl;
+    cout << "p_score[0]: " << p_score[0] << endl;
 
     return 0;
 }
