2d6a90f0a6775df4f01a3ad36d94a341168fb614
Ali Aryani <lenesis@proton.me>
2025-09-19 23:41:26 +0330

    array of const ptr const char -> declare array; print out array

--- 41-array-of-const-ptr-const-char/array-of-const-ptr-const-char.cpp
+++ 41-array-of-const-ptr-const-char/array-of-const-ptr-const-char.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main () {
+    const char* students [] {
+        "John Doe",
+        "Frank Sinatra",
+        "Nancy Sinatra",
+        "Doris Day",
+        "Edith Piaf"
+    };
+    
+    // print out the students
+    cout << endl;
+    cout << "Printing out the students:" << endl;
+    for (const char* student: students) {
+        cout << student << endl;
+    }
 
     return 0;
 }
