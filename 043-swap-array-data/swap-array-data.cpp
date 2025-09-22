713ba982d15d535fd260829d06df36b5b8452bab
Ali Aryani <lenesis@proton.me>
2025-09-22 09:30:02 +0330

    swap array data -> declare and print out arrays

--- 043-swap-array-data/swap-array-data.cpp
+++ 043-swap-array-data/swap-array-data.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
+    int arr0[]{1,2,3,4,5};
+    int arr1[]{6,7,8,9,10};
+
+    // print arr0
+    cout << "arr0: ";
+    for (size_t i{}; i < size(arr0); ++i) {
+        cout << arr0[i] << " ";
+    }
+    cout << endl;
+
+    // print arr1
+    cout << "arr1: ";
+    for (size_t i: arr1) {
+        cout << i << " ";
+    }
+    cout << endl;
 
     return 0;
 }
