ea0392da8d517ea7a296259932b9d9d9e038388f
Ali Aryani <lenesis@proton.me>
2025-09-22 09:38:08 +0330

    swap array data -> swap the hard way

--- 043-swap-array-data/swap-array-data.cpp
+++ 043-swap-array-data/swap-array-data.cpp
@@ -1,10 +1,40 @@
+#include <cstddef>
 #include <iostream>
+#include <iterator>
 using namespace std;
 
 int main() {
     int arr0[]{1,2,3,4,5};
     int arr1[]{6,7,8,9,10};
 
+    // print arr0
+    cout << "arr0: ";
+    for (size_t i{}; i < size(arr0); ++i) {
+        cout << arr0[i] << " ";
+    }
+    cout << endl;
+
+    // print arr1
+    cout << "arr1: ";
+    for (size_t i: arr1) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // swapping arrays the hard way:
+    int temp[5];
+    // move data from arr1 to temp
+    for (size_t i{0};i < size(arr1); ++i) {
+        temp[i] = arr1[i];
+    }
+    // move data from arr0 to arr1
+    for (size_t i{}; i < size(arr0); ++i) {
+        arr1[i] = arr0[i];
+    }
+    // move data from temp to arr0
+    for(size_t i{};i<size(temp);++i){
+        arr0[i] = temp[i];
+    }
     // print arr0
     cout << "arr0: ";
     for (size_t i{}; i < size(arr0); ++i) {
