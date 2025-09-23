690f77a83b717d7ed8aa02b18faa0ec55fd8610e
Ali Aryani <lenesis@proton.me>
2025-09-23 21:10:42 +0330

    pointer arithmetic -> navigate using increment

--- 044-pointer-arithmetic/pointer-arithmetic.cpp
+++ 044-pointer-arithmetic/pointer-arithmetic.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main() {
+    int scores[10] = {11,12,13,14,15,16,17,18,19,20};
+    int* p_score {scores};
 
+    cout << "values in scores array (p_score pointer increment):" << endl;
+    cout << "Address: " << p_score << " value: " << *p_score << endl;
+    ++p_score; // moves forward by sizeof(int) : 4 Bytes
+    cout << "Address: " << p_score << " value: " << *p_score << endl;
+    ++p_score; // moves forward by sizeof(int) : 4 Bytes
+    cout << "Address: " << p_score << " value: " << *p_score << endl;
+    ++p_score; // moves forward by sizeof(int) : 4 Bytes
+    cout << "Address: " << p_score << " value: " << *p_score << endl;
+    ++p_score; // moves forward by sizeof(int) : 4 Bytes
+    cout << "Address: " << p_score << " value: " << *p_score << endl;
+    ++p_score; // moves forward by sizeof(int) : 4 Bytes
+    cout << "Address: " << p_score << " value: " << *p_score << endl;
 
     return 0;
 }
