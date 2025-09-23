7627588c884d2d260741b87127d57a7f6381c974
Ali Aryani <lenesis@proton.me>
2025-09-23 21:19:12 +0330

    pointer arithmetic -> explicit navigation

--- 044-pointer-arithmetic/pointer-arithmetic.cpp
+++ 044-pointer-arithmetic/pointer-arithmetic.cpp
@@ -18,5 +18,11 @@ int main() {
     ++p_score; // moves forward by sizeof(int) : 4 Bytes
     cout << "Address: " << p_score << " value: " << *p_score << endl;
 
+    // explicit increment
+    cout << endl;
+    cout << "Explicit addition of integer:" << endl;
+    p_score = scores; // reseting 
+    cout << "scores[4]: " << *(p_score + 4) << endl;
+    
     return 0;
 }
