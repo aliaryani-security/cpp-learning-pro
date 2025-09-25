ed0bced3552ad22fb2a89e460793887c86d8e448
Ali Aryani <lenesis@proton.me>
2025-09-25 17:20:49 +0330

    when new fails -> two failures

--- 046-when-new-fails/when-new-fails.cpp
+++ 046-when-new-fails/when-new-fails.cpp
@@ -1,7 +1,14 @@
+#include <cstddef>
 #include <iostream>
 using namespace std;
 
 int main() {
-
+    // int* data = new int[10000000000000];
+    /*
+    for(size_t i{0}; i < 1000000000;++i) {
+        int* data = new int[1000000000];
+    } 
+    */
+    cout << "program ending well!" << endl;
     return 0;
 }
