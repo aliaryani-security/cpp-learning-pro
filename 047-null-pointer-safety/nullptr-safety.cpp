eb80334038117a3269554ad838a2ab6ccd04b70b
Ali Aryani <lenesis@proton.me>
2025-09-25 22:40:52 +0330

    nullptr safety -> a better impelementation of verbose mode

--- 047-null-pointer-safety/nullptr-safety.cpp
+++ 047-null-pointer-safety/nullptr-safety.cpp
@@ -8,13 +8,21 @@ int main() {
     int* p_number{}; //initialised to nullptr
     p_number = new int(7);
     
-    if(!(p_number==nullptr)){
+/*    if(!(p_number==nullptr)){
         cout << "p_number points to a valid address: " << p_number << endl;
         cout << "*p_number: " << *p_number << endl;
     }else{
         cout << "p_number points to an invalid address" << endl;
     }
-
+    delete p_number;
+    p_number = nullptr;
+    */
+    if(p_number){
+        cout << "p_number points to a valid address: " << p_number << endl;
+        cout << "*p_number: " << *p_number << endl;
+    }else{
+        cout << "p_number points to an invalid address" << endl;
+    }
     delete p_number;
     p_number = nullptr;
 
