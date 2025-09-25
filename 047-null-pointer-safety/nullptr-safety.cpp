a5f0cbe0a1f312ea2fffbeec19e750b38b2451c8
Ali Aryani <lenesis@proton.me>
2025-09-25 22:31:06 +0330

    nullptr safety -> verbose mode

--- 047-null-pointer-safety/nullptr-safety.cpp
+++ 047-null-pointer-safety/nullptr-safety.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
+    // verbose nullptr check
+    cout << "verbose nullptr check:" << endl;
+
+    int* p_number{}; //initialised to nullptr
+    
+    if(!(p_number==nullptr)){
+        cout << "p_number points to a valid address: " << p_number << endl;
+    }else{
+        cout << "p_number points to an invalid address" << endl;
+    }
 
     return 0;
 }
