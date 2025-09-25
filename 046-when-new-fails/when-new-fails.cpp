7bb279bf94f15f2c5a48828a69aed87122d15675
Ali Aryani <lenesis@proton.me>
2025-09-25 22:11:23 +0330

    when new fails -> nothrow

--- 046-when-new-fails/when-new-fails.cpp
+++ 046-when-new-fails/when-new-fails.cpp
@@ -1,5 +1,7 @@
 #include <cstddef>
+#include <exception>
 #include <iostream>
+#include <new>
 using namespace std;
 
 int main() {
@@ -9,6 +11,25 @@ int main() {
         int* data = new int[1000000000];
     } 
     */
+
+/*    for(size_t i{0}; i < 1000000000; ++i) {
+        try {
+            int* data = new int[1000000000];
+        }catch(exception& ex){
+            cout << "something went wrong: " << ex.what() << endl;
+        }
+    }*/
+    //exception: std::nothrow
+    for (size_t i{0};i < 100;++i) {
+        int* data = new(nothrow) int[10000000000000];
+        if (data != nullptr) {
+            cout << "data allocated." << endl;
+        }else{
+            cout << "data allocation failed." << endl;
+        }
+    }
+    
     cout << "program ending well!" << endl;
+
     return 0;
 }
