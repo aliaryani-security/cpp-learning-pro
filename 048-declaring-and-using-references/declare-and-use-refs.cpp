d5323fe4b0f8b5d86c496e946ff1275e70c15e5d
Ali Aryani <lenesis@proton.me>
2025-09-26 16:17:01 +0330

    declare and use refs -> declare and print out

--- 048-declaring-and-using-references/declare-and-use-refs.cpp
+++ 048-declaring-and-using-references/declare-and-use-refs.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main() {
+    int int_data {33};
+    double double_data {55};
+
+    int& ref_int_data{int_data};
+    double& ref_double_data{double_data};
+
+    cout << "int_data: " << int_data << endl;
+    cout << "&int_data: " << &int_data << endl;
+    cout << "double_data: " << double_data << endl;
+    cout << "&double_data: " << &double_data << endl;
+
+    cout << "ref_int_data: " << ref_int_data << endl;
+    cout << "&ref_int_data: " << &ref_int_data << endl;
+    cout << "ref_double_data: " << ref_double_data << endl;
+    cout << "&ref_double_data: " << &ref_double_data << endl;
 
     return 0;
 }
